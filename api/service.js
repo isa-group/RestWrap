3f28bfaeb55c7a963ec6f01b4fd1c8e6113b0bc9
davbrican <72500941+davbrican@users.noreply.github.com>
2022-08-09 11:34:42 +0200

    Update service.js

--- api/service.js
+++ api/service.js
@@ -1,9 +1,31 @@
 const axios = require('axios');
+require("dotenv").config();
+
+authorization_token=process.env.ACCESS_TOKEN;
+api_url = 'https://api.github.com/repos/isa-group/datasets/contents/plans/';
+
+
 
 module.exports.getAll = (req, res) => {
-    res.send({message: "All Ok"});
+    axios.get(api_url, {headers: {Authorization: authorization_token}})
+    .then(response => {
+        console.log(response.data);
+        res.send({message: "All Ok", data: response.data});
+    })
+    .catch(error => {
+        console.log(error);
+        res.send({message: "Error", data: error});
+    });
 }
 
 module.exports.get = (req, res) => {
-    res.send({message: "1 Ok"});
+    axios.get(api_url+req.params.service+"-sla4oai.yaml", {headers: {Authorization: authorization_token}})
+    .then(response => {
+        console.log(response.data);
+        res.send({message: "1 Ok", data: response.data});
+    })
+    .catch(error => {
+        console.log(error);
+        res.send({message: "Error", data: error});
+    });
 }
