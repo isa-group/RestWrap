880f43c6ee87e7785e7c5c1bbcfe954906b7a2a4
davbrican <72500941+davbrican@users.noreply.github.com>
2022-08-29 12:58:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,5 @@ app.use('/api/v1', router);
 
 const port = process.env.PORT || 8080;
 app.listen(port, function() {
-    console.log("App running at http://localhost:" + port);
-    console.log("________________________________________________________________");
+    console.log(`Server ready at port ${port}`);
 });
